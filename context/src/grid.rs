61ede6cf0f8da406802e82c3637c079e0c42ca8d
Horup <sah@sah.dk>
2022-10-23 19:49:07 +0200

    refactored

---
+++ context/src/grid.rs
@@ -0,0 +1,44 @@
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Grid<T:Clone + Default> {
+    size:usize,
+    cells:Vec<T>
+}
+
+impl<T> Default for Grid<T> where T:Default+Clone {
+    fn default() -> Self {
+        let size = 64;
+        Self { 
+            size,
+            cells:vec![T::default();size * size]
+         }
+    }
+}
+
+impl<T> Grid<T> where T:Default+Clone {
+    pub fn size(&self)->usize {
+        return self.size;
+    }
+
+    pub fn get_mut(&mut self, x:i32, y:i32) -> Option<&mut T> {
+        let size = self.size;
+        if x >= 0 && y >= 0 {
+            let index = y as usize * size + x as usize;
+            return self.cells.get_mut(index);
+        }
+
+        return None;
+    }
+
+    pub fn get(&self,x:i32, y:i32) -> Option<&T> {
+        let size = self.size;
+        if x >= 0 && y >= 0 {
+            let index = y as usize * size + x as usize;
+            return self.cells.get(index);
+        }
+
+        return None;
+    }
+}
--- context/src/lib.rs
+++ context/src/lib.rs
@@ -4,47 +4,6 @@ pub struct Tile {
     pub texture:u32
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Grid<T:Clone + Default> {
-    size:usize,
-    cells:Vec<T>
-}
-
-impl<T> Default for Grid<T> where T:Default+Clone {
-    fn default() -> Self {
-        let size = 64;
-        Self { 
-            size,
-            cells:vec![T::default();size * size]
-         }
-    }
-}
-
-impl<T> Grid<T> where T:Default+Clone {
-    pub fn size(&self)->usize {
-        return self.size;
-    }
-
-    pub fn get_mut(&mut self, x:i32, y:i32) -> Option<&mut T> {
-        let size = self.size;
-        if x >= 0 && y >= 0 {
-            let index = y as usize * size + x as usize;
-            return self.cells.get_mut(index);
-        }
-
-        return None;
-    }
-
-    pub fn get(&self,x:i32, y:i32) -> Option<&T> {
-        let size = self.size;
-        if x >= 0 && y >= 0 {
-            let index = y as usize * size + x as usize;
-            return self.cells.get(index);
-        }
-
-        return None;
-    }
-}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Entity {
@@ -110,8 +69,9 @@ mod map;
 pub use map::*;
 mod edit;
 pub use edit::*;
+mod grid;
+pub use grid::*;
 
 use glam::Vec2;
 
 pub use glam;
-use serde::{Serialize, Deserialize};
