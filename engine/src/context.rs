0e8c1d0763677451e1503244aaa2dfc68dd14563
Søren Alsbjerg Hørup <sah@sah.dk>
2022-11-10 22:05:43 +0100

    refactored

--- engine/src/context.rs
+++ engine/src/context.rs
@@ -92,16 +92,28 @@ impl Context for Engine {
         Vec2::new(0.0, 0.0)
     }
 
-    fn draw_string(&self, _params: shared::DrawStringParams) {
-        todo!()
+    fn draw_string(&self, p: shared::DrawStringParams) {
+        draw_text_ex(&p.str, p.x, p.y, TextParams {
+            ..Default::default()
+        });
     }
 
-    fn draw_texture(&self, _params: shared::DrawTextureParams) {
-        todo!()
+    fn draw_texture(&self, p: shared::DrawTextureParams) {
+        if let Some(tex) = self.textures.get(&p.texture) {
+            draw_texture_ex(tex.clone(), p.x, p.y, WHITE, DrawTextureParams {
+                dest_size:Some(Vec2::new(p.w, p.h)),
+                ..Default::default()
+            });
+        }
     }
 
-    fn draw_rect(&self, _params: shared::DrawRectParams) {
-        todo!()
+    fn draw_rect(&self, params: shared::DrawRectParams) {
+        draw_rectangle(params.x, params.y, params.w, params.h, Color {
+            r:params.color.r,
+            g:params.color.g,
+            b:params.color.b,
+            a:params.color.a
+        })
     }
 
     fn push_command(&mut self, command: shared::Command) {
